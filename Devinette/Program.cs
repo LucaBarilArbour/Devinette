f859ccdb98835ce6861942c8a1947584579a26ad
LucaBarilArbour <2563230@cegepoutaouais.qc.ca>
2026-08-25 16:55:18 -0400

    Terminer le program de devinette

--- Devinette/Program.cs
+++ Devinette/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿public class Program
+{
+    static int GenereNombreAleatoir()
+    {
+        Random random = new Random();
+        int min = 0;
+        int max = 100;
+        return random.Next(min, max);
+    }
+
+    static void DevinerNombre()
+    {
+        int nombreADeviner = GenereNombreAleatoir();
+        int nbEssai = 0;
+        int guess;
+        do
+        {
+            Console.Write("Entre un nombre : ");
+            while (!int.TryParse(Console.ReadLine(), out guess))
+            {
+                Console.WriteLine("Entrée invalide, réessaie.");
+            }
+            nbEssai++;
+            if (guess > nombreADeviner)
+            {
+                Console.WriteLine("Le nombre est plus petit");
+            }
+            else if (guess < nombreADeviner)
+            {
+                Console.WriteLine("Le nombre est plus grand");
+            }
+            else
+            {
+                Console.WriteLine("Tu a bien deviner");
+            }
+            ;
+        } while (nbEssai <= 10);
+    }
+
+    static void Main(string[] args)
+    {
+        DevinerNombre();
+    }
+}
